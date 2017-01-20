3730a8b7304065fcc6a2f53e7c3e200365bb2f79
Tahir Dawson <dawsontahir@gmail.com>
2017-01-20 14:20:52 -0500

    Number Wizard Game- Switching Levels

---
+++ NumberWizardUI/Assets/LayerManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LayerManager : MonoBehaviour {
+
+	public void SwitchScene(string name){
+		Application.LoadLevel (name);
+	}
+
+	public void QuitGame (){
+
+		Application.Quit ();
+	}
+}
